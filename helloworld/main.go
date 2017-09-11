659a247d2a7a76e7dac71c821d15cc3699c66d15
Brett <brett.coomer@investec.co.za>
2017-09-11 18:10:43 +0200

    feat: helloworld

---
+++ helloworld/main.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+Executable package type generates an executable file
+Reusable packages are used as helpers (libraries, components, etc)
+
+name of package determines package type:
+- main = executable (must have a main function)
+- any other name - reusable
+*/
+
+import "fmt"
+
+/*
+import libraries from other packages
+fmt (format) = standard library package
+by default main has access to no libraries unless imported
+golang.org/pkg
+*/
+
+func main() {
+	fmt.Println("Hi there!")
+}
