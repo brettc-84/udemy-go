a3de421445880f9e356c94d3e0e5e4cdb3db35c4
brettc-84 <brett.coomer@gmail.com>
2017-09-16 13:39:37 +0200

    slice

--- cards/main.go
+++ cards/main.go
@@ -5,10 +5,18 @@ import "fmt"
 func main() {
 	// var card string = "Ace of spades"
 	// card := "Ace of spades" // type string is inferred by assignment
-	card := newCard()
+	// card := newCard()
 	// card = "Five of diamonds"
 
-	fmt.Println(card)
+	// fmt.Println(card)
+
+	// slice
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+	fmt.Println(cards)
 }
 
 func newCard() string {
