d958c9d0b40281114af6331fdd5795d3124d12df
Brett.Coomer <Brett.Coomer@investec.co.za>
2019-07-22 09:54:58 +0200

    feat: nested struct

--- structs/main.go
+++ structs/main.go
@@ -2,15 +2,21 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipcode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
 	age       int
+	contact   contactInfo
 }
 
 func main() {
-	alex := person{"Alex", "Anderson", 32}
-	tony := person{firstName: "Tony", lastName: "Sly", age: 41}
+	alex := person{"Alex", "Anderson", 32, contactInfo{"alex.a@gmail.com", 1234}}
+	tony := person{firstName: "Tony", lastName: "Sly", age: 41, contact: contactInfo{email: "tony@sly.com", zipcode: 91021}}
 	var dexter person
 
 	dexter.firstName = "Dexter"
