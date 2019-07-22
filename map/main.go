6504becbbff19fa5528c02dd09ce2e4775edd306
Brett.Coomer <Brett.Coomer@investec.co.za>
2019-07-22 13:10:26 +0200

    feat: maps

--- map/main.go
+++ map/main.go
@@ -7,7 +7,8 @@ func main() {
 	colours := make(map[string]string)
 	colours = map[string]string{
 		"red":   "#ff0000",
-		"green": "##008000",
+		"green": "#008000",
+		"black": "unknown",
 	}
 
 	colours["white"] = "#ffffff"
@@ -15,6 +16,8 @@ func main() {
 	// delete(colours, "red")
 
 	// fmt.Println(colours)
+	// printMap(colours)
+	updateMap(colours)
 	printMap(colours)
 }
 
@@ -22,4 +25,8 @@ func printMap(c map[string]string) {
 	for key, value := range c {
 		fmt.Println("Hex code for", key, "is", value)
 	}
+}
+
+func updateMap(c map[string]string) {
+	c["black"] = "#000000"
 }
