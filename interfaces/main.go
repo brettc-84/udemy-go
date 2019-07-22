c0d066f66efb5dc83635341c6cdf5872804b1126
Brett.Coomer <Brett.Coomer@investec.co.za>
2019-07-22 13:54:44 +0200

    feat: interface!

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,32 +2,40 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
 	eb := englishBot{}
-	// sb := spanishBot{}
+	sb := spanishBot{}
 
 	printGreeting(eb)
-	// printGreeting(sb)
+	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGretting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
+
+// func printGreeting(eb englishBot) {
+// 	fmt.Println(eb.getGretting())
+// }
 
 // func printGreeting(sb spanishBot) {
 // 	fmt.Println(sb.getGretting())
 // }
 
 // when not using the receiver variable, it can be ommitted
-func (englishBot) getGretting() string {
+func (englishBot) getGreeting() string {
 	// custom logic for english bot implementation
 	return "Hello there"
 }
 
-func (spanishBot) getGretting() string {
+func (spanishBot) getGreeting() string {
 	// custom logic for spanish bot implementation
 	return "Ola!"
 }
